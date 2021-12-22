150b0b986b8d218d74478f466e880b2cf3d9a71e
Thomas Pham <thang.pham@techvsi.com>
2021-12-23 02:01:37 +0700

    update json file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "app",
-        script: "node ./app.js",
+        script: "./app.js",
         instances: "max",
         env: {
             NODE_ENV: "development",
