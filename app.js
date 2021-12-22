1aa5a5a8d35828b1a65677312f2284ef17a13588
Thomas Pham <thang.pham@techvsi.com>
2021-12-23 01:33:51 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-const http = require('http');
-const port = process.env.PORT || 3001
+const http = require('http')
+const fs = require('fs')
 
+const port = process.env.PORT || 3001
+const helperfunc = require('./helper-functions')
 
 const server = http.createServer((req, res) => {
-    console.log('ok')
+    console.log(req.url)
+
+
+    let path_json = './apmt/request_sample/'
     res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.writeHead(200, {'Content-Type': 'application/json'});
 
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Hello world!</h1>')
+    if (req.url == '/') {
+        res.write('<h1>Hello world!</h1>')
 
+        res.end()
+    } else if (req.url === '/get-avail-slot') {
+        var data = fs.readFileSync(path_json + 'getavailslot.json', 'utf-8')
+        data = helperfunc.convert_string_to_json_stringify(data)
+        res.write(data);
+        res.end(data)
+    } else if (req.url === '/list-branch') {
+        var data = fs.readFileSync(path_json + 'listbranch.json', 'utf-8')
+        data = helperfunc.convert_string_to_json_stringify(data)
+        res.write(data);
+        res.end(data)
+
+    } else if (req.url === '/list-service-web') {
+        var data = fs.readFileSync(path_json + 'listserviceweb.json', 'utf-8')
+        data = helperfunc.convert_string_to_json_stringify(data)
+        res.write(data)
+        res.end(data)
+    } else {
+        res.write('<html>')
+        res.write('<title>Error</title>')
+        res.write('<h1>404 Not Found</h1>')
+        res.write('</html>')
+        res.end()
+    }
 
 })
 
 server.listen(port, () => {
-    console.log('oktest')
     console.log("Server running at port " + port)
 })
---
+++ helper-functions.js
@@ -0,0 +1,12 @@
+function convert_string_to_json_stringify(data) {
+    //parse string to json object
+    data = JSON.parse(data)
+    console.log(typeof data)
+    //It's because data's data type is an object/json, and res.write() accepts data type string or buffer.
+    // JSON.stringify converts the object to a string basically.
+    data = JSON.stringify(data)
+    return data
+}
+
+
+module.exports = {convert_string_to_json_stringify}
